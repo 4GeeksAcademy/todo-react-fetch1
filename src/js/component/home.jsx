33d1a147c58dca100fee6e7468dd42922fbc1a73
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-07-07 19:51:44 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos]=useState([])
+	const [input, setInput]=useState({})
+
+	useEffect(()=>{
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //here is where your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        //error handling
+        console.log(error);
+    });
+	},[])
+
+	const addTodo=()=>{
+		setTodos([...todos,input])
+	}
+	const inputChange=(event)=>{
+		setInput({label:event.target.value, done:false})
+	}
+
+	const removeTodo=(index)=>{
+		setTodos((todo)=>{
+			return todo.filter((item, i)=>i !=index)
+		})
+	}
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 };
